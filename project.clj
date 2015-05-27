ae7f372ab54927af886c13c9b54d9544eeb8f2bf
Daniel Fitzpatrick <doubleagent03@gmail.com>
2015-05-26 21:01:32 -0500

    Switch to Leiningen

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject math.numeric-tower "0.0.6-SNAPSHOT"
+  :description "Math functions that deal intelligently with the various types in Clojure's numeric tower, as well as math functions commonly found in Scheme implementations."
+  :url "git@github.com:clojure/math.numeric-tower.git"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
